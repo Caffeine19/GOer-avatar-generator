e25b3b3238583b1bf46366143106254ab680e772
Caffeine19 <939597201@qq.com>
2023-03-02 15:39:10 +0800

    :sparkles: 完成EditorSection组件的分离和 折叠展开的动画

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -3,22 +3,14 @@
 import ColorSelector from './ColorSelector.vue'
 import Slider from './Slider.vue'
 import Divider from './Divider.vue'
+import EditorSection from './EditorSection.vue'
 </script>
 <template>
   <div
     class="shadow-slate-900/5 rounded-xl border-slate-100 h-full p-4 space-y-4 overflow-y-auto bg-white border shadow-xl"
   >
-    <div name="section" class="space-y-3">
-      <div name="section-title" class="flex items-center justify-between">
-        <div class="items-c flex space-x-3">
-          <i class="ph-palette" style="font-size: 28px"></i>
-          <h2 class="cartograph-cf-regular-italic text-slate-900 text-xl">Color/颜色</h2>
-        </div>
-        <button class="flex items-center justify-center">
-          <i class="ph-caret-down text-slate-400" style="font-size: 20px"></i>
-        </button>
-      </div>
-      <div name="section-content" class="pl-1 space-y-3">
+    <EditorSection title-icon="ph-palette" title-text="Color/颜色">
+      <template #content>
         <div class="space-y-1.5">
           <h3 class="cartograph-cf-regular text-slate-700">primary color/首要颜色</h3>
           <ColorSelector></ColorSelector>
@@ -31,20 +23,12 @@ import Divider from './Divider.vue'
           <h3 class="cartograph-cf-regular text-slate-700">background color/背景颜色</h3>
           <ColorSelector></ColorSelector>
         </div>
-      </div>
-    </div>
+      </template>
+    </EditorSection>
     <Divider></Divider>
-    <div name="section" class="space-y-3">
-      <div name="section-title" class="flex items-center justify-between">
-        <div class="items-c flex space-x-3">
-          <i class="ph-atom" style="font-size: 28px"></i>
-          <h2 class="cartograph-cf-regular-italic text-slate-900 text-xl">Eyes/眼睛</h2>
-        </div>
-        <button class="flex items-center justify-center">
-          <i class="ph-caret-down text-slate-400" style="font-size: 20px"></i>
-        </button>
-      </div>
-      <div name="section-content" class="pl-1 space-y-3">
+
+    <EditorSection title-icon="ph-atom" title-text="Eyes/眼睛">
+      <template v-slot:content>
         <div class="space-y-2">
           <h3 class="cartograph-cf-regular text-slate-800 text-base">right/右</h3>
           <div class="space-y-2">
@@ -62,8 +46,6 @@ import Divider from './Divider.vue'
             </div>
           </div>
         </div>
-      </div>
-      <div name="section-content" class="pl-1 space-y-3">
         <div class="space-y-2">
           <h3 class="cartograph-cf-regular text-slate-800 text-base">left/左</h3>
           <div class="space-y-2">
@@ -81,63 +63,40 @@ import Divider from './Divider.vue'
             </div>
           </div>
         </div>
-      </div>
-      <p class="text-slate-300 text-xs font-light">*大小眼往往被作为判定高智慧人士的依据之一</p>
-    </div>
+      </template>
+      <template v-slot:remark>
+        <p class="text-slate-300 text-xs font-light">*大小眼往往被作为判定高智慧人士的依据之一</p>
+      </template>
+    </EditorSection>
     <Divider></Divider>
-    <div name="section" class="space-y-3">
-      <div name="section-title" class="flex items-center justify-between">
-        <div class="items-c flex space-x-3">
-          <i class="ph-drop" style="font-size: 28px"></i>
-          <h2 class="cartograph-cf-regular-italic text-slate-900 text-xl">Effect/特效</h2>
-        </div>
-        <button class="flex items-center justify-center">
-          <i class="ph-caret-down text-slate-400" style="font-size: 20px"></i>
-        </button>
-      </div>
 
-      <div name="section-content" class="pl-1 space-y-3">
+    <EditorSection title-icon="ph-magic-wand" title-text="Effect/特效">
+      <template v-slot:content>
         <div class="flex items-center justify-between space-x-4">
           <h4 class="cartograph-cf-regular text-slate-700">Blur</h4>
           <Slider class="basis-4/5"></Slider>
         </div>
-      </div>
-      <p class="text-slate-300 text-xs font-light">
-        *我在制作的过程中，保留了一部分模糊，因为我觉得，只有保留一部分模糊，才知道你用的是网图（骄傲）.jpg
-      </p>
-    </div>
+      </template>
+      <template v-slot:remark
+        ><p class="text-slate-300 text-xs font-light">
+          *我在制作的过程中，保留了一部分模糊，因为我觉得，只有保留一部分模糊，才知道你用的是网图（骄傲）.jpg
+        </p></template
+      >
+    </EditorSection>
     <Divider></Divider>
-    <div name="section" class="space-y-3">
-      <div name="section-title" class="flex items-center justify-between">
-        <div class="items-c flex space-x-3">
-          <i class="ph-planet" style="font-size: 28px"></i>
-          <h2 class="cartograph-cf-regular-italic text-slate-900 text-xl">Round Corner/圆角</h2>
-        </div>
-        <button class="flex items-center justify-center">
-          <i class="ph-caret-down text-slate-400" style="font-size: 20px"></i>
-        </button>
-      </div>
 
-      <div name="section-content" class="pl-1 space-y-3">
+    <EditorSection title-icon="ph-planet" title-text="Round Corner/圆角">
+      <template v-slot:content>
         <div class="flex items-center justify-between space-x-4">
           <h4 class="cartograph-cf-regular text-slate-700">Radius</h4>
           <Slider class="basis-4/5"></Slider>
         </div>
-      </div>
-    </div>
+      </template>
+    </EditorSection>
     <Divider></Divider>
-    <div name="section" class="space-y-3">
-      <div name="section-title" class="flex items-center justify-between">
-        <div class="items-c flex space-x-3">
-          <i class="ph-code" style="font-size: 28px"></i>
-          <h2 class="cartograph-cf-regular-italic text-slate-900 text-xl">Code/咒语</h2>
-        </div>
-        <button class="flex items-center justify-center">
-          <i class="ph-caret-down text-slate-400" style="font-size: 20px"></i>
-        </button>
-      </div>
 
-      <div name="section-content" class="pl-1">
+    <EditorSection title-icon="ph-code" title-text="Code/咒语">
+      <template v-slot:content>
         <pre class="">
             <code class="language-json rounded-lg">
 {
@@ -170,8 +129,7 @@ import Divider from './Divider.vue'
 }
             </code>
         </pre>
-      </div>
-    </div>
+      </template>
+    </EditorSection>
   </div>
 </template>
-<style></style>
---
+++ src/components/EditorSection.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{
+  titleIcon: string
+  titleText: string
+}>()
+
+const isSectionContentCollapsed = ref(true)
+
+const toggleSectionContent = () => {
+  isSectionContentCollapsed.value = !isSectionContentCollapsed.value
+}
+</script>
+<template>
+  <div name="section" class="space-y-3">
+    <div name="section-title" class="flex items-center justify-between">
+      <div class="flex items-center space-x-3">
+        <i :class="titleIcon" style="font-size: 28px"></i>
+        <h2 class="cartograph-cf-regular-italic text-slate-900 text-xl">{{ titleText }}</h2>
+      </div>
+      <button
+        class="flex items-center justify-center transition-transform duration-300"
+        @click="toggleSectionContent"
+        :class="isSectionContentCollapsed ? 'rotate-0' : 'rotate-180'"
+      >
+        <i class="ph-caret-down text-slate-400" style="font-size: 20px"></i>
+      </button>
+    </div>
+    <div
+      name="section-content"
+      class="pl-1 space-y-3 overflow-hidden transition-all duration-300"
+      :class="isSectionContentCollapsed ? 'max-h-0' : 'max-h-screen'"
+    >
+      <slot name="content" :isSectionContentCollapsed="isSectionContentCollapsed"> </slot>
+    </div>
+    <slot name="remark"></slot>
+  </div>
+</template>
