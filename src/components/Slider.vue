a77981fde523a0edba692be09294790e0faaa03a
Caffeine19 <939597201@qq.com>
2023-03-03 11:08:08 +0800

    :sparkles: 完成slider组件的封装

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,16 +1,76 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+
+const percent = ref()
+const props = withDefaults(defineProps<{ min?: number; max?: number; range?: number }>(), {
+  min: 0,
+  max: 100,
+  range: 50
+})
+const localRange = ref(props.range)
+const emits = defineEmits<{
+  (e: 'updateRange', value: number): void
+}>()
+watch(
+  localRange,
+  () => {
+    percent.value = Math.floor(((localRange.value - props.min) / (props.max - props.min)) * 100)
+    emits('updateRange', localRange.value)
+  },
+  { immediate: true }
+)
+</script>
 <template>
   <div class="flex items-center space-x-3">
-    <div class="border-slate-200 w-full h-5 border rounded">
-      <div class="bg-slate-100 relative w-4/5 h-5">
-        <div class="bg-slate-300 -right-2 top-1/2 h-7 absolute w-4 -translate-y-1/2 rounded"></div>
-      </div>
+    <div class="grow flex items-center">
+      <input
+        type="range"
+        id="volume"
+        name="volume"
+        :min="min"
+        :max="max"
+        class="border-slate-200 opacity-60 w-full h-5 border rounded"
+        v-model="localRange"
+        :style="{
+          'background-image': `-webkit-gradient(
+            linear,
+            left top,
+            right top,
+            color-stop(${percent}%, #f1f5f9),
+            color-stop(${percent}%, #fff)
+          )`
+        }"
+      />
     </div>
     <div
-      class="border-slate-200 flex items-center justify-center w-6 h-6 overflow-hidden border rounded"
+      class="border-slate-200 flex items-center justify-center w-10 h-6 overflow-hidden border rounded"
     >
-      <p class="text-slate-700 text-base">18</p>
+      <p class="text-slate-700 text-base">{{ localRange }}</p>
     </div>
   </div>
 </template>
+
+<style scoped>
+input[type='range'] {
+  -webkit-appearance: none;
+
+  /* background-image: -webkit-gradient(
+    linear,
+    left top,
+    right top,
+    color-stop(15%, #f1f5f9),
+    color-stop(15%, #fff)
+  ); */
+}
+
+input[type='range']:focus {
+  outline: none;
+  @apply opacity-100;
+}
+
+input[type='range']::-webkit-slider-thumb {
+  -webkit-appearance: none !important;
+  @apply bg-slate-300 h-7 w-4 rounded;
+}
+</style>
